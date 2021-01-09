61a18970bf25792ed57f18edbb29b6b6351c5f1f
keren <kv5171@bs.amalnet.k12.il>
2021-01-09 22:03:28 +0200

    End App 1

---
+++ app/src/main/java/com/example/ex01/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ex01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn);
+    }
+
+    public void change(View view) {
+        btn.setText("Oh, yea, I’ve been clicked!");
+    }
+}
